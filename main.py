06e0f94d9bfa2b5f971b3eaf06c788094571397e
kbrudnowski <krzysztof.brudnowski@gmail.com>
2023-05-13 16:56:15 +0200

    Add database url for heroku postgres

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def admin_only(f):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
